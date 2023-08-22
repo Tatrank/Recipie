01ea8963e589b8cef14ab561a6a892ab19c4941f
Tatranka <vaclav.dousa28@post.cz>
2023-08-22 09:42:07 +0200

    added infinity scroll on one page

--- app/recipe/user/view_users/page.tsx
+++ app/recipe/user/view_users/page.tsx
@@ -1,65 +1,67 @@
 "use client";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useRef } from "react";
-import { useIntersection } from "@mantine/hooks";
-interface UserData {
-  name: string;
-}
 
-const something = [{ name: "me" }];
+import { useEffect, useRef, useState } from "react";
 
+// Usage example:
 export default function Page() {
-  const { data, fetchNextPage } = useInfiniteQuery(
-    ["query"],
-    async ({ pageParam = 0 }) => {
-      const res = await fetch(
-        "http://localhost:3000/api/all_users?page=" + pageParam,
-        { cache: "no-store" }
-      );
+  const targetRef = useRef<HTMLDivElement>(null);
+  const [isAtEnd, setIsAtEnd] = useState(false);
+  const [data, setData] = useState<{ name: string }[]>([]);
+  const [noMoreFetches, setNoMoreFetches] = useState(false);
+  const [page, setPage] = useState(0);
 
-      return (await res.json()) as Promise<UserData[]>;
-    },
-    {
-      getNextPageParam: (_, pages) => {
-        return pages.length;
+  useEffect(() => {
+    console.log(noMoreFetches);
+    fetch("http://localhost:3000/api/all_users?page=" + page)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.length === 0) {
+          setNoMoreFetches(true);
+          return;
+        }
+        setData((prevData) => [...prevData, ...res]);
+      });
+  }, [page]);
+
+  useEffect(() => {
+    console.log("změna targetu");
+    const target = targetRef.current;
+    if (!target) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+
+        if (entry.isIntersecting && !noMoreFetches) {
+          setPage((prevPage) => prevPage + 1);
+        }
       },
-    }
-  );
+      {
+        root: null, // Observe within viewport
+        rootMargin: "0px",
+        threshold: 1,
+      }
+    );
 
-  const last_item = useRef<HTMLElement>(null);
-  const { ref, entry } = useIntersection({
-    root: last_item.current,
-    threshold: 1,
-  });
-  if (entry?.isIntersecting) {
-    fetchNextPage();
-  }
+    observer.observe(target);
+
+    return () => {
+      observer.unobserve(target);
+    };
+  }, [targetRef, noMoreFetches]);
 
   return (
-    <>
-      <>ssddf</>
-      {data?.pages.map((page, i) => (
-        <div key={i}>
-          {page.map((item, id) => {
-            if ((id = page.length)) {
-              return (
-                <div
-                  className="h-[80vh] w-[100vh]
-              "
-                  ref={ref}
-                >
-                  {item.name}
-                </div>
-              );
-            }
-            return (
-              <div className="h-[120vh]" key={item.name}>
-                {item.name}
-              </div>
-            );
-          })}
-        </div>
-      ))}
-    </>
+    <div>
+      <div className="w-[100vw] bg-accent-light h-[150vh]"></div>
+      {/* Your content */}
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>{item.name}</div>
+        ))}
+      </div>
+      <div ref={targetRef}></div>
+    </div>
   );
 }
--- components/SessionProvid.tsx
+++ components/SessionProvid.tsx
@@ -1,21 +1,11 @@
 "use client";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MantineProvider } from "@mantine/core";
+
 export default function SessionProvid({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-  return (
-    <SessionProvider>
-      <MantineProvider>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </MantineProvider>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
